616614ab425490064d30a507599c54279ea70513
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-10-25 16:46:48 -0500

    tel

--- app/src/main/java/mx/ipn/intent2project/MainActivity.java
+++ app/src/main/java/mx/ipn/intent2project/MainActivity.java
@@ -1,9 +1,14 @@
 package mx.ipn.intent2project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +19,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Llamar(View view) {
-        
+        Intent intent;
+        Uri uri;
+        uri=Uri.parse("tel:5522300603");
+        intent= new Intent(Intent.ACTION_CALL, uri);
+        if(ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.CALL_PHONE)== PackageManager.PERMISSION_GRANTED
+        ){
+            startActivity(intent);
+        }else{
+            Toast.makeText(this,"no se puede no hay tortillas",Toast.LENGTH_LONG).show();
+        }
     }
 }
